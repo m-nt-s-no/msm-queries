f7f0a05b405df8baf2618ece66988e54b3c3a51c
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-11-26 03:41:04 +0000

    added RCAV for youngest, oldest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,4 +8,26 @@ class DirectorsController < ApplicationController
     end
     render({:template => "/all_directors"})
   end
+
+  def youngest
+    latest_dob = Director.maximum(:dob)
+    @dob = latest_dob.strftime("%B %-d, %Y")
+    youngest = Director.where(:dob => latest_dob)
+    @youngest_name = youngest.at(0).name
+    @youngest_id = youngest.at(0).id
+    render({:template => "/youngest"})
+  end
+
+  def oldest
+    earliest_dob = Director.minimum(:dob)
+    @dob = earliest_dob.strftime("%B %-d, %Y")
+    oldest = Director.where(:dob => earliest_dob)
+    @oldest_name = oldest.at(0).name
+    @oldest_id = oldest.at(0).id
+    render({:template => "/oldest"})
+  end
+
+  def show_director
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "show_all" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "oldest" })
+  get("/directors/:id", { :controller => "directors", :action => "show_director" })
 end
